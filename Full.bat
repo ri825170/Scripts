f3d69439ffa0678a2295199d7228b95e6db3952b
ri825170 <56911765+ri825170@users.noreply.github.com>
2019-10-24 14:41:38 +0100

    Create Full.bat

---
+++ Full.bat
@@ -0,0 +1,8 @@
+# Little Script to disconnect OpenVpn from Split Tunnel and connect to Full Tunnel automatically
+
+@ echo off
+cd C:\Program Files\OpenVPN\bin
+Taskkill /IM openvpn.exe /F
+Taskkill /IM openvpn-gui.exe /F
+cd C:\Program Files\OpenVPN\bin
+openvpn-gui --connect VPN-Full.ovpn
