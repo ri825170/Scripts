e300eaf8c52e1be7ba83b378506744ac05ef3f24
ri825170 <56911765+ri825170@users.noreply.github.com>
2019-10-24 16:27:40 +0100

    Update Full.bat

--- Full.bat
+++ Full.bat
@@ -5,4 +5,4 @@ cd C:\Program Files\OpenVPN\bin
 Taskkill /IM openvpn.exe /F
 Taskkill /IM openvpn-gui.exe /F
 cd C:\Program Files\OpenVPN\bin
-openvpn-gui --connect VPN-Full.ovpn
+openvpn-gui --connect VPN-config_file_name.ovpn
